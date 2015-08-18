9ea2e98c6f1d3dbdf4b460d00d77849b9ba286ba
Jesse Pimenta <drrrreams@gmail.com>
2015-08-18 01:39:49 -0600

    setting up fb profile routing

--- easyAuth/models/routes.js
+++ easyAuth/models/routes.js
@@ -20,7 +20,11 @@ module.exports = function(app, passport) {
 
     // process the login form
     // app.post('/login', do all our passport stuff here);
-
+    app.post('/login', passport.authenticate('local-login', {
+          successRedirect : '/profile', // redirect to the secure profile section
+          failureRedirect : '/login', // redirect back to the signup page if there is an error
+          failureFlash : true // allow flash messages
+      }));
     // =====================================
     // SIGNUP ==============================
     // =====================================
@@ -50,6 +54,20 @@ module.exports = function(app, passport) {
             user : req.user // get the user out of session and pass to template
         });
     });
+
+    // =====================================
+    // FACEBOOK ROUTES =====================
+    // =====================================
+    // route for facebook authentication and login
+    app.get('/auth/facebook', passport.authenticate('facebook', { scope : 'email' }));
+
+    // handle the callback after facebook has authenticated the user
+    app.get('/auth/facebook/callback',
+        passport.authenticate('facebook', {
+            successRedirect : '/profile',
+            failureRedirect : '/'
+        }));
+
 
     // =====================================
     // LOGOUT ==============================
