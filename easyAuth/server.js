c6c1ec2bfeff64a4b450c7c2e7651802c22b0bec
Jesse Pimenta <drrrreams@gmail.com>
2015-08-19 21:03:54 -0600

    removed the port

--- easyAuth/server.js
+++ easyAuth/server.js
@@ -40,5 +40,5 @@ app.use(flash()); // use connect-flash for flash messages stored in session
 require('../easyAuth/models/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 
 // launch ======================================================================
-app.listen(port);
-console.log('The magic happens on port ' + port);
+app.listen(app.get('port'));
+console.log('The magic happens on port ');
